54fae376dbaabf088ae1d4832995a3456dc52373
Tommy4ever <54824749+Tommy4ever@users.noreply.github.com>
2019-09-17 16:39:11 -0400

    Add files via upload

---
+++ task.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * @author Tommy
+ *
+ */
+import java.io.StringWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class task {
+	public static void main(String[] args) {
+
+	StringWriter sw = new StringWriter();
+	PrintWriter pw = new PrintWriter (sw);
+	
+	pw.print("Hungry");
+	
+	String name = sw.toString();
+	
+	System.out.println(name);
+	
+	 String myWords = "Tommy  26";
+	 Scanner in = new Scanner(myWords);
+	 
+	 String name1 = in.next ();
+	 int age = in.nextInt();
+	 
+	 System.out.printf("%s is %d years old", name1, age);
+	 
+	 
+	 
+	
+	
+	
+	
+ }
+ }
