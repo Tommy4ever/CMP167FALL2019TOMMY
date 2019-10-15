85dc35828c1f6165a52ce4fdfae0dc509b1214ca
Tommy4ever <54824749+Tommy4ever@users.noreply.github.com>
2019-10-15 16:38:05 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Animal {
+	public int numLegs; 
+	private int age; 
+	public void bark() {
+	System.out.println("Woof-woof");
+	}
+	public void run() {
+		
+	}
+}
